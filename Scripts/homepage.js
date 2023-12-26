bc8ef6b6c05d39ae95ff29b4249d246fdaf1bd1e
Guptasai <154473964+Guptasai@users.noreply.github.com>
2023-12-26 12:43:15 -0500

    Add files via upload

--- Scripts/homepage.js
+++ Scripts/homepage.js
@@ -3,10 +3,12 @@ let imgTag = document.getElementById("turn");
 let spanTags = document.querySelectorAll("span");
 let divTag = document.getElementById("winner");
 let playAgain = document.getElementById("playAgain");
+let xImg = "https://Guptasai.github.io/Tic-Tac-Toe/Images/x.png";
+let oImg = "https://Guptasai.github.io/Tic-Tac-Toe/Images/o.png";
 let winningRows = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 let clickedCount = 0;
 function changeTurn(){
-    imgTag.src = (imgTag.getAttribute("src")=="/Images/x.png")?"/Images/o.png":"/Images/x.png";
+    imgTag.src = (imgTag.getAttribute("src")==xImg)?oImg:xImg;
 }
 function stopPlay(){
     for (let i=0;i<spanTags.length;i++){
@@ -37,18 +39,19 @@ function eventListeners(){
     clickedCount = 0;
     divTag.textContent = "";
     playAgain.style.display = "none";
-    imgTag.src = "/Images/x.png";
+    imgTag.src = xImg;
     for (let i=0;i<spanTags.length;i++){
-        spanTags[i].className = ";NA";
+        spanTags[i].className = "NA";
         spanTags[i].innerHTML = ""
         spanTags[i].addEventListener("click",play)
     }
 }
     function play(event){
     let image = document.createElement("img");
-    image.src = imgTag.getAttribute("src");
+    image.src = imgTag.src;
+    image.alt = imgTag.alt;
     event.target.appendChild(image);
-    event.target.className = imgTag.getAttribute("src")[8];
+    event.target.className = imgTag.getAttribute("src")[7];
     check(event.target);
     changeTurn();
     event.target.removeEventListener("click",play)
